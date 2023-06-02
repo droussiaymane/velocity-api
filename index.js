40993a2eafc709edc81fb077a56cb0e27d1ddd36
droussiaymane <aymanedroussi.off@gmail.com>
2023-06-02 18:53:28 +0100

    asd

--- index.js
+++ index.js
@@ -18,6 +18,60 @@ app.post('/test-integration', (req, res) => {
 });
 
 
+app.post('/issuing/identify', (req, res) => {
+
+
+  const users=[
+    {
+      "id": "1",
+      "name": "John Doe",
+      "email": "adev.off@gmail.com",
+      "phone": "08123456789",
+      "address": "Jl. Jend. Sudirman No. 1",
+      "dob": "1990-01-01",
+
+
+    },
+    {
+      "id": "2",
+      "name": "John Doe 2",
+      "email": "aymanedroussi.contact@gmail.com",
+      "phone": "08123456789",
+      "address": "Jl. Jend. Sudirman No. 1",
+      "dob": "1990-01-01",
+    }
+
+  ]
+
+  const userBody= req.body
+
+  const user = users.find(user => user.email === userBody.emails[0]);
+
+  if(!user){
+    res.status(404).json({message:"user not found"})
+  }
+  else{
+    const payload = {
+      "id": user.id,
+      "name": user.name,
+      "email": user.email,
+      "phone": user.phone,
+      "address": user.address,
+      "dob": user.dob,
+      "photo": "https://i.ibb.co/7t7zFZJ/IMG-20201202-161547.jpg",
+
+
+    }
+
+    res.json(payload);
+  }
+
+
+});
+
+
+
+
 
 // Start the server
 app.listen(3000, () => {
